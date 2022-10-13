ce546b2e6a961a071845718dc4ce563adfdf1995
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-10-13 08:58:01 -0500

    Update test script

--- recipe/test_stack_imports.py
+++ recipe/test_stack_imports.py
@@ -1,12 +1,18 @@
-from openff.interchange.components.interchange import Interchange
+from openff.toolkit import Molecule, ForceField
 from openff.toolkit.utils.toolkits import (
     GLOBAL_TOOLKIT_REGISTRY,
     AmberToolsToolkitWrapper,
     RDKitToolkitWrapper,
 )
 
+
 assert RDKitToolkitWrapper().is_available()
 assert AmberToolsToolkitWrapper().is_available()
 
-
 print(GLOBAL_TOOLKIT_REGISTRY.registered_toolkit_versions)
+
+molecule = Molecule.from_smiles("CCO")
+molecule.generate_conformers()
+force_field = ForceField("openff-2.0.0.offxml")
+
+force_field.create_openmm_system(molecule.to_topology())
