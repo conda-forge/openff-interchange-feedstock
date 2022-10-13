4673d3d1cfe8b9632d1efabbf7236b874d4ee4e1
Matthew W. Thompson <mattwthompson@protonmail.com>
2022-10-13 14:39:23 -0500

    Debug import errors in test

--- recipe/test_stack_imports.py
+++ recipe/test_stack_imports.py
@@ -1,9 +1,6 @@
-from openff.toolkit import Molecule, ForceField
-from openff.toolkit.utils.toolkits import (
-    GLOBAL_TOOLKIT_REGISTRY,
-    AmberToolsToolkitWrapper,
-    RDKitToolkitWrapper,
-)
+from openff.toolkit import *
+from openff.interchange import *
+from openff.toolkit.utils.toolkits import *
 
 
 assert RDKitToolkitWrapper().is_available()
