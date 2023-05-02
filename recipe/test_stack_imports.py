f580878e730c815dd346b44b1f9754e7adbd75bc
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-05-02 13:50:40 -0500

    Try patching high-level `__init__.py`

--- recipe/test_stack_imports.py
+++ recipe/test_stack_imports.py
@@ -1,15 +1,25 @@
+import sys
+
 from openff.toolkit import *
 from openff.interchange import *
 from openff.toolkit.utils.toolkits import *
 
+for thing in [
+    'Interchange',
+    'ForceField',
+    'Molecule',
+    'Topology',
+]:
+    assert thing in sys.modules
 
 assert RDKitToolkitWrapper().is_available()
 assert AmberToolsToolkitWrapper().is_available()
 
 print(GLOBAL_TOOLKIT_REGISTRY.registered_toolkit_versions)
 
-molecule = Molecule.from_smiles("CCO")
+molecule = Molecule.from_smiles("C")
 molecule.generate_conformers()
 force_field = ForceField("openff-2.0.0.offxml")
 
 force_field.create_openmm_system(molecule.to_topology())
+Interchange.from_smirnoff(force_field, [molecule])
