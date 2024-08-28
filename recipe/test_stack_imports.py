84b888044e13f4711c45f516eb458084e1bc4e21
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-08-28 11:13:12 -0500

    Update test script

--- recipe/test_stack_imports.py
+++ recipe/test_stack_imports.py
@@ -1,6 +1,16 @@
-from openff.interchange import *
-from openff.toolkit import *
-from openff.toolkit.utils.toolkits import *
+from openff.interchange import Interchange
+from openff.toolkit import ForceField, Molecule, Topology, Quantity
+from openff.toolkit.utils.toolkits import (
+    RDKitToolkitWrapper,
+    OpenEyeToolkitWrapper,
+    AmberToolsToolkitWrapper,
+    BuiltInToolkitWrapper,
+    ToolkitWrapper,
+    GLOBAL_TOOLKIT_REGISTRY,
+)
+import openmm
+import openmm.unit
+
 
 for thing in [
     "Interchange",
@@ -26,8 +36,18 @@ molecule = Molecule.from_smiles("C")
 molecule.generate_conformers()
 force_field = ForceField("openff-2.0.0.offxml")
 
-interchange = force_field.create_interchange(molecule.to_topology())
-interchange.to_openmm()
+topology = molecule.to_topology()
+topology.box_vectors = Quantity([4, 4, 4], "nanometer")
+
+interchange = force_field.create_interchange(topology)
+
+interchange.to_openmm_simulation(
+    integrator=openmm.LangevinMiddleIntegrator(
+        293.15 * openmm.unit.kelvin,
+        1.0 / openmm.unit.picosecond,
+        2.0 * openmm.unit.femtosecond,
+    )
+)
 interchange.to_gromacs("fOOO")
 interchange.to_lammps("bAAAR")
-interchange.to_prmtop("bAAAZ")
+interchange.to_prmtop("bAAAZ")  # replace with to_amber when it exists ...
